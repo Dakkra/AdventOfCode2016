b712ee4d8badc769af71968ead4c8a3300a50178
Dakkra <contact@dakkra.com>
2016-12-05 18:08:04 -0700

    Almost... so close

--- DayThree/src/com/dakkra/advent/Main.java
+++ DayThree/src/com/dakkra/advent/Main.java
@@ -1,6 +1,5 @@
 package com.dakkra.advent;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,9 +10,10 @@ public class Main {
     private static LinkedList<Triangle> triangles = new LinkedList<>();
     private static InputStream inputStream;
     private static BufferedReader bufferedReader;
+    private static int realTriangleCount = 0;
 
     public static void main(String[] args) {
-        inputStream = Main.class.getResourceAsStream("input2");
+        inputStream = Main.class.getResourceAsStream("input");
         bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         try {
             boolean doneReading = false;
@@ -24,20 +24,42 @@ public class Main {
                     continue;
                 }
                 assesLine(lineBuffer);
-                System.out.println(lineBuffer);
             }
             bufferedReader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Num triangle: " + triangles.size());
+
+        for (Triangle t : triangles) {
+            if (t.isRealTriangle())
+                realTriangleCount++;
+        }
+        System.out.println("Real triangle: " + realTriangleCount);
     }
 
     private static void assesLine(String lineBuffer) {
         int num[] = new int[3];
-        int index = 0;
-        String stringBuff = "";
+        int numIndex = 0;
+        int charIndex = 0;
         char charBuff;
-        //TODO process this shit.
+        char lineBufChars[] = lineBuffer.toCharArray();
+        String stringBuff = "";
+
+        while (numIndex <= 2) {
+            while ((charBuff = lineBufChars[charIndex++]) == ' ' && charIndex < lineBufChars.length) {
+                //literally do nothing here...
+            }
+            stringBuff += charBuff;
+            if (charIndex < lineBufChars.length)
+                while ((charBuff = lineBufChars[charIndex++]) != ' ' && charIndex < lineBufChars.length) {
+                    stringBuff += charBuff;
+                }
+            num[numIndex] = Integer.parseInt(stringBuff);
+            numIndex++;
+            stringBuff = "";
+        }
+        triangles.add(new Triangle(num[0], num[1], num[2]));
     }
 
     static class Triangle {
@@ -50,7 +72,11 @@ public class Main {
         }
 
         public boolean isRealTriangle() {
-            return (l + w) > h;
+            boolean testOne, testTwo, testThree;
+            testOne = (l + w) >= h;
+            testTwo = (l + h) >= w;
+            testThree = (w + h) > l;
+            return (testOne && testTwo && testThree);
         }
     }
 
